938a382e4d7ed5bd407ee2f0ae37aaee50cf744a
BlackKnight017 <blackknight.oct1@gmail.com>
2023-06-29 19:41:20 -0700

    Update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 #     plt.subplot(330+1+i)
 #     plt.imshow(train_X[i])
 #     plt.show()
-
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -54,11 +54,49 @@ model.compile(loss='categorical_crossentropy',
 
 model.summary()
 
-model.fit(train_X,train_Y,
-    validation_data=(test_X,test_Y),
-    epochs=10,batch_size=32)
+# model.fit(train_X,train_Y,
+#     validation_data=(test_X,test_Y),
+#     epochs=1,batch_size=32)
 
-acc=model.evaluate(test_X,test_Y)
-print(acc*100)
+# acc=model.evaluate(test_X,test_Y)
+# print(acc*100)
 
-model.save("model1_cifar_10epoch.h5")
+# model.save("model1_cifar_10epoch.h5")
+loaded_model = tf.keras.models.load_model("model1_cifar_10epoch.h5")
+results={
+   0:'aeroplane',
+   1:'automobile',
+   2:'bird',
+   3:'cat',
+   4:'deer',
+   5:'dog',
+   6:'frog',
+   7:'horse',
+   8:'ship',
+   9:'truck'
+}
+from PIL import Image
+import numpy as np
+im=Image.open("cat1.png")
+# the input image is required to be in the shape of dataset, i.e (32,32,3)
+ 
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+img_path = 'cat1.png'
+img = image.load_img(img_path, target_size=(32, 32))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+# Make predictions
+preds = loaded_model.predict(x)
+
+# Convert the predictions to human-readable labels
+decoded_preds = decode_predictions(preds, top=3)[0]
+print(decoded_preds)
+
+# im=im.resize((32,32))
+# im=np.expand_dims(im,axis=0)
+# im=np.array(im)
+# pred=model.predict_step([im])[0]
+# print(pred,results[pred])
